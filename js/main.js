16fcf0ef12e0ab856fcdf2dd26f56b6d2c458a2c
Kieran Poropat <kieran.poropat@gmail.com>
2024-06-27 20:31:41 -0400

    carousel portfolio, port changes

---
+++ js/main.js
@@ -0,0 +1,131 @@
+
+// -----------------------< Variables>-------------------------------
+
+let sketchesNextBtn = document.querySelector(".sketchesBtn-next");
+let sketchesPrevBtn = document.querySelector(".sketchesBtn-prev");
+
+let coverArtPrevBtn = document.querySelector(".coverArtBtn-prev");
+let coverArtNextBtn = document.querySelector(".coverArtBtn-next");
+
+let OWPrevBtn = document.querySelector(".owBtn-prev");
+let OWNextBtn = document.querySelector(".owBtn-next");
+
+let sketchesImage = document.querySelector(".sketches__img");
+let coverArtImg = document.querySelector(".cover-art__img");
+let offWhiteImg = document.querySelector(".off-white__img");
+
+
+// -----------------------< DOM CONTENT LOADED >-------------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+// -----------------------< Image References >-------------------------------
+
+    let imageData = [
+        [
+            { src: "../content/digitized/vinny-redone.jpg" },
+            { src: "../content/digitized/owen-redone.jpg" },
+            { src: "../content/digitized/ninja-redone.jpg" },
+            { src: "../content/digitized/extra-redone.jpg" },
+            { src: "../content/digitized/broly-redone.jpg" },
+            { src: "../content/digitized/gogeta-pencil-redone.jpg" },
+            { src: "../content/digitized/gogeta-color-redone.jpg" },
+            { src: "../content/digitized/future-trunks-color-redone.jpg" }
+        ],
+        [
+            { src: "../content/Nike x Off White ipad mockup.png" },
+            { src: "../content/nike off white iphone mockup 2.png" },
+            { src: "../content/nike x off white iphone mockup.png" }
+        ],
+        [
+            { src: "../content/12 AM COVER.jpg" },
+            { src: "../content/ETERNITY COVER FINAL EP2.jpg" },
+            { src: "../content/FLASHBACK COVER FINISHED.jpg" },
+            { src: "../content/FAULTED COVER.jpg" }
+        ]
+    ];
+
+// -----------------------< Indexes >-------------------------------
+
+    let currentIndexSketches = 0;
+    let currentIndexOffWhite = 0;
+    let currentIndexCoverArt = 0;
+
+// -----------------------< Update Functions >-------------------------------
+
+    function updateSketches() {
+        sketchesImage.src = imageData[0][currentIndexSketches].src;
+    }
+
+    function updateOffWhite() {
+        offWhiteImg.src = imageData[1][currentIndexOffWhite].src;
+    }
+
+    function updateCoverArt() {
+        coverArtImg.src = imageData[2][currentIndexCoverArt].src;
+    }
+
+
+   // -----------------------< Sketches >-------------------------------
+
+    sketchesNextBtn.addEventListener('click', () => {
+        currentIndexSketches++;
+        if (currentIndexSketches >= imageData[0].length) {
+            currentIndexSketches = 0;
+        }
+        updateSketches();
+    });
+
+    sketchesPrevBtn.addEventListener('click', () => {
+        currentIndexSketches--;
+        if (currentIndexSketches < 0) {
+            currentIndexSketches = imageData[0].length - 1;
+        }
+        updateSketches();
+    });
+
+ // -----------------------< Off White >-------------------------------
+
+    OWNextBtn.addEventListener('click', () => {
+        currentIndexOffWhite++;
+        if (currentIndexOffWhite >= imageData[1].length) {
+            currentIndexOffWhite = 0;
+        }
+        updateOffWhite();
+    });
+
+    OWPrevBtn.addEventListener('click', () => {
+        currentIndexOffWhite--;
+        if (currentIndexOffWhite < 0) {
+            currentIndexOffWhite = imageData[1].length - 1;
+        }
+        updateOffWhite();
+    });
+
+// -----------------------< Cover Art >-------------------------------
+
+    coverArtNextBtn.addEventListener('click', () => {
+        currentIndexCoverArt++;
+        if (currentIndexCoverArt >= imageData[2].length) {
+            currentIndexCoverArt = 0;
+        }
+        updateCoverArt();
+    });
+
+    coverArtPrevBtn.addEventListener('click', () => {
+        currentIndexCoverArt--;
+        if (currentIndexCoverArt < 0) {
+            currentIndexCoverArt = imageData[2].length - 1;
+        }
+        updateCoverArt();
+    });
+
+
+
+    // called functions to update
+
+    updateOffWhite();
+    updateSketches();
+    updateCoverArt();
+});
